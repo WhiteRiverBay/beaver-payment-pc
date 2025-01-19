a87fdf2fa2b679d395b2b5425bebda35ad957d32
Ethan <zhangyuxin85@gmail.com>
2025-01-18 19:51:22 -0800

    update-2025-01-18-19-51-22-for-cursor

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Buffer as BufferPolyfill } from 'buffer'
+import { Buffer } from 'buffer'
 
-// 使用 polyfill
-globalThis.Buffer = BufferPolyfill
+globalThis.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
